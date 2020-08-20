7687a51a54928d626c7d142faaec665d6fd52ae4
Thomas Stussi <tstussi06@gmail.com>
2020-08-20 13:33:55 -0700

    update data folder for spells and favorites

---
+++ data/favorites.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    name: 'Fireball',
+    level: 3,
+    desc: 'this go big boom',
+  },
+  {
+    name: 'Polymorph',
+    level: 4,
+    desc: 'you wanna drink like an animal?',
+  },
+  {
+    name: 'Identify',
+    level: 1,
+    desc: 'spell that makes the world go round',
+  },
+];
